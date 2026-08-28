5d32b54709fc63371cc8e207cc698857e3a79034
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-27 22:36:44 -0300

    fix: correção variavel do tipo da etapa

--- internal/core/domain/lead/funil.go
+++ internal/core/domain/lead/funil.go
@@ -12,4 +12,14 @@ type Etapa struct {
 	Nome    string    `db:"nome"`
 	Ordem  int       `db:"ordem"`
 	FunilID uuid.UUID `db:"funil_id"`
+	Tipo TipoEtapa `db:"tipo_etapa"`
 }
+
+
+type TipoEtapa string
+
+const (
+	TipoEtapaContato  = "CONTATO"
+	TipoEtapaPerdido = "PERDIDO"
+	TipoEtapaFechado = "FECHADO"
+)
